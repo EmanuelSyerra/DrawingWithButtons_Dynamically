1668b7347f31fb94a59e20ceedd817a42a65a576
Emanuel Syerra <emanueldeveloping@gmail.com>
2020-11-01 14:56:03 -0600

    KANJI KAME 亀

---
+++ Arrays - DrawingWithButtons/Form.Designer.cs
@@ -0,0 +1,136 @@
+﻿namespace Arrays___DrawingWithButtons
+{
+    partial class Form
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnCrear = new System.Windows.Forms.Button();
+            this.btnLimpiar = new System.Windows.Forms.Button();
+            this.btnNaranja = new System.Windows.Forms.Button();
+            this.btnCirculo = new System.Windows.Forms.Button();
+            this.btnEscudo = new System.Windows.Forms.Button();
+            this.btnBordar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnCrear
+            // 
+            this.btnCrear.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnCrear.Location = new System.Drawing.Point(12, 12);
+            this.btnCrear.Name = "btnCrear";
+            this.btnCrear.Size = new System.Drawing.Size(75, 23);
+            this.btnCrear.TabIndex = 0;
+            this.btnCrear.Text = "Crear";
+            this.btnCrear.UseVisualStyleBackColor = true;
+            this.btnCrear.Click += new System.EventHandler(this.Crear_Click);
+            // 
+            // btnLimpiar
+            // 
+            this.btnLimpiar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnLimpiar.Location = new System.Drawing.Point(12, 12);
+            this.btnLimpiar.Name = "btnLimpiar";
+            this.btnLimpiar.Size = new System.Drawing.Size(75, 23);
+            this.btnLimpiar.TabIndex = 1;
+            this.btnLimpiar.Text = "Limpiar";
+            this.btnLimpiar.UseVisualStyleBackColor = true;
+            this.btnLimpiar.Click += new System.EventHandler(this.Limpiar_Click);
+            // 
+            // btnNaranja
+            // 
+            this.btnNaranja.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnNaranja.Location = new System.Drawing.Point(93, 12);
+            this.btnNaranja.Name = "btnNaranja";
+            this.btnNaranja.Size = new System.Drawing.Size(75, 23);
+            this.btnNaranja.TabIndex = 2;
+            this.btnNaranja.Text = "Naranja";
+            this.btnNaranja.UseVisualStyleBackColor = true;
+            this.btnNaranja.Click += new System.EventHandler(this.Naranja_Click);
+            // 
+            // btnCirculo
+            // 
+            this.btnCirculo.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnCirculo.Location = new System.Drawing.Point(174, 12);
+            this.btnCirculo.Name = "btnCirculo";
+            this.btnCirculo.Size = new System.Drawing.Size(75, 23);
+            this.btnCirculo.TabIndex = 3;
+            this.btnCirculo.Text = "Circulo";
+            this.btnCirculo.UseVisualStyleBackColor = true;
+            this.btnCirculo.Click += new System.EventHandler(this.btnCirculo_Click);
+            // 
+            // btnEscudo
+            // 
+            this.btnEscudo.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnEscudo.Location = new System.Drawing.Point(255, 12);
+            this.btnEscudo.Name = "btnEscudo";
+            this.btnEscudo.Size = new System.Drawing.Size(75, 23);
+            this.btnEscudo.TabIndex = 4;
+            this.btnEscudo.Text = "Escudo";
+            this.btnEscudo.UseVisualStyleBackColor = true;
+            this.btnEscudo.Click += new System.EventHandler(this.btnEscudo_Click);
+            // 
+            // btnBordar
+            // 
+            this.btnBordar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnBordar.Location = new System.Drawing.Point(336, 12);
+            this.btnBordar.Name = "btnBordar";
+            this.btnBordar.Size = new System.Drawing.Size(75, 23);
+            this.btnBordar.TabIndex = 5;
+            this.btnBordar.Text = "Bordar";
+            this.btnBordar.UseVisualStyleBackColor = true;
+            this.btnBordar.Click += new System.EventHandler(this.btnBordar_Click);
+            // 
+            // Form
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(614, 611);
+            this.Controls.Add(this.btnBordar);
+            this.Controls.Add(this.btnEscudo);
+            this.Controls.Add(this.btnCirculo);
+            this.Controls.Add(this.btnNaranja);
+            this.Controls.Add(this.btnLimpiar);
+            this.Controls.Add(this.btnCrear);
+            this.MaximumSize = new System.Drawing.Size(630, 650);
+            this.MinimumSize = new System.Drawing.Size(630, 650);
+            this.Name = "Form";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnCrear;
+        private System.Windows.Forms.Button btnLimpiar;
+        private System.Windows.Forms.Button btnNaranja;
+        private System.Windows.Forms.Button btnCirculo;
+        private System.Windows.Forms.Button btnEscudo;
+        private System.Windows.Forms.Button btnBordar;
+    }
+}
+
---
+++ Arrays - DrawingWithButtons/Form.cs
@@ -0,0 +1,430 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Arrays___DrawingWithButtons
+{
+    public partial class Form : System.Windows.Forms.Form
+    {
+        private int x = 12;
+        private int y = 40;
+        private int cont = 0;
+        private int filas = 37;
+        private int columnas = 39;
+        Button[,] pixels = new Button[37, 39];
+
+        public Form()
+        {
+            InitializeComponent();
+        }
+
+        private void Botones(bool valor)
+        {
+            this.btnCrear.Visible = !valor;
+            this.btnLimpiar.Visible = valor;
+            this.btnNaranja.Visible = valor;
+            this.btnCirculo.Visible = valor;
+            this.btnEscudo.Visible = valor;
+            this.btnBordar.Visible = valor;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Botones(false);
+        }
+
+        private void Crear_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i < this.filas; i++)
+            {
+                for (int j = 0; j < this.columnas; j++)
+                {
+                    
+                    Button pixel = new Button();
+
+                    
+                    pixel.FlatStyle = FlatStyle.Flat;
+                    pixel.Location = new Point(x, y);
+                    pixel.Name = "btn" + cont.ToString();
+                    pixel.Size = new Size(15, 15);
+                    this.x += 15;
+                    pixel.Text = "";
+                    pixel.UseVisualStyleBackColor = true;
+
+                    this.pixels[i, j] = pixel;
+
+                    Controls.Add(pixels[i, j]);
+                    this.cont++;
+                }
+
+                this.x = 12;
+                this.y += 15;
+
+            }
+            Botones(true);
+        }
+
+        private void Limpiar_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i < this.filas; i++)
+            {
+                for (int j = 0; j < this.columnas; j++)
+                {
+                    this.pixels[i, j].BackColor = Color.White;
+                    this.pixels[i, j].FlatAppearance.BorderColor = Color.White;
+                }
+            }
+        }
+
+        private void Naranja_Click(object sender, EventArgs e)
+        {
+            for(int i = 0; i < this.filas; i++)
+            {
+                for(int j = 0; j < this.columnas; j++)
+                {
+                    if(i == 0 || i == 36)
+                    {
+                        if(j < 14 || j > 24)
+                        {
+                            this.pixels[i, j].BackColor = Color.Orange;
+                            this.pixels[i, j].FlatAppearance.BorderColor = Color.Orange;
+                        }
+                    }
+                    if (i == 1 || i == 35)
+                    {
+                        if (j < 11 || j > 27)
+                        {
+                            this.pixels[i, j].BackColor = Color.Orange;
+                            this.pixels[i, j].FlatAppearance.BorderColor = Color.Orange;
+                        }
+                    }
+                    if (i == 2 || i == 34)
+                    {
+                        if (j < 9 || j > 29)
+                        {
+                            this.pixels[i, j].BackColor = Color.Orange;
+                            this.pixels[i, j].FlatAppearance.BorderColor = Color.Orange;
+                        }
+                    }
+                    if (i == 3 || i == 33)
+                    {
+                        if (j < 8 || j > 30)
+                        {
+                            this.pixels[i, j].BackColor = Color.Orange;
+                            this.pixels[i, j].FlatAppearance.BorderColor = Color.Orange;
+                        }
+                    }
+                    if (i == 4 || i == 32)
+                    {
+                        if (j < 6 || j > 32)
+                        {
+                            this.pixels[i, j].BackColor = Color.Orange;
+                            this.pixels[i, j].FlatAppearance.BorderColor = Color.Orange;
+                        }
+                    }
+                    if (i == 5 || i == 31)
+                    {
+                        if (j < 5 || j > 33)
+                        {
+                            this.pixels[i, j].BackColor = Color.Orange;
+                            this.pixels[i, j].FlatAppearance.BorderColor = Color.Orange;
+                        }
+                    }
+                    if (i == 6 || i == 7 || i == 29 || i == 30)
+                    {
+                        if (j < 4 || j > 34)
+                        {
+                            this.pixels[i, j].BackColor = Color.Orange;
+                            this.pixels[i, j].FlatAppearance.BorderColor = Color.Orange;
+                        }
+                    }
+                    if (i == 8 || i == 28)
+                    {
+                        if (j < 3 || j > 35)
+                        {
+                            this.pixels[i, j].BackColor = Color.Orange;
+                            this.pixels[i, j].FlatAppearance.BorderColor = Color.Orange;
+                        }
+                    }
+                    if (i == 9 || i == 10 || i == 26 || i == 27)
+                    {
+                        if (j < 2 || j > 36)
+                        {
+                            this.pixels[i, j].BackColor = Color.Orange;
+                            this.pixels[i, j].FlatAppearance.BorderColor = Color.Orange;
+                        }
+                    }
+                    if (i == 11 || i == 12 || i == 13 || i == 23 || i == 24 || i == 25)
+                    {
+                        if (j < 1 || j > 37)
+                        {
+                            this.pixels[i, j].BackColor = Color.Orange;
+                            this.pixels[i, j].FlatAppearance.BorderColor = Color.Orange;
+                        }
+                    }
+                }
+            }
+        }
+
+        private void btnCirculo_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i < this.filas; i++)
+            {
+                for (int j = 0; j < this.columnas; j++)
+                {
+                    if (i == 0 || i == 36)
+                    {
+                        if (j > 13 && j < 25)
+                        {
+                            this.pixels[i, j].BackColor = Color.Black;
+                            this.pixels[i, j].FlatAppearance.BorderColor = Color.Black;
+                        }
+                    }
+                    if (i == 1 || i == 35)
+                    {
+                        if (j > 10 && j < 28)
+                        {
+                            this.pixels[i, j].BackColor = Color.Black;
+                            this.pixels[i, j].FlatAppearance.BorderColor = Color.Black;
+                        }
+                    }
+                    if (i == 2 || i == 34)
+                    {
+                        if (j > 8 && j < 14 || j > 24 && j < 30)
+                        {
+                            this.pixels[i, j].BackColor = Color.Black;
+                            this.pixels[i, j].FlatAppearance.BorderColor = Color.Black;
+                        }
+                    }
+                    if (i == 3 || i == 33)
+                    {
+                        if (j > 7 && j < 12 || j > 26 && j < 31)
+                        {
+                            this.pixels[i, j].BackColor = Color.Black;
+                            this.pixels[i, j].FlatAppearance.BorderColor = Color.Black;
+                        }
+                    }
+                    if (i == 4 || i == 32)
+                    {
+                        if (j > 5 && j < 10 || j > 28 && j < 33)
+                        {
+                            this.pixels[i, j].BackColor = Color.Black;
+                            this.pixels[i, j].FlatAppearance.BorderColor = Color.Black;
+                        }
+                    }
+                    if (i == 5 || i == 31)
+                    {
+                        if (j > 4 && j < 8 || j > 30 && j < 34)
+                        {
+                            this.pixels[i, j].BackColor = Color.Black;
+                            this.pixels[i, j].FlatAppearance.BorderColor = Color.Black;
+                        }
+                    }
+                    if (i == 6 || i == 30)
+                    {
+                        if (j > 3 && j < 7 || j > 31 && j < 35)
+                        {
+                            this.pixels[i, j].BackColor = Color.Black;
+                            this.pixels[i, j].FlatAppearance.BorderColor = Color.Black;
+                        }
+                    }
+                    if (i == 7 || i == 29)
+                    {
+                        if (j > 3 && j < 6 || j > 32 && j < 35)
+                        {
+                            this.pixels[i, j].BackColor = Color.Black;
+                            this.pixels[i, j].FlatAppearance.BorderColor = Color.Black;
+                        }
+                    }
+                    if (i == 8 || i == 28)
+                    {
+                        if (j > 2 && j < 5 || j > 33 && j < 36)
+                        {
+                            this.pixels[i, j].BackColor = Color.Black;
+                            this.pixels[i, j].FlatAppearance.BorderColor = Color.Black;
+                        }
+                    }
+                    if (i == 9 || i == 27)
+                    {
+                        if (j > 1 && j < 5 || j > 33 && j < 37)
+                        {
+                            this.pixels[i, j].BackColor = Color.Black;
+                            this.pixels[i, j].FlatAppearance.BorderColor = Color.Black;
+                        }
+                    }
+                    if (i == 10 || i == 26)
+                    {
+                        if (j > 1 && j < 4 || j > 34 && j < 37)
+                        {
+                            this.pixels[i, j].BackColor = Color.Black;
+                            this.pixels[i, j].FlatAppearance.BorderColor = Color.Black;
+                        }
+                    }
+                    if (i > 10 && i < 14 || i > 22 && i < 26)
+                    {
+                        if (j > 0 && j < 3 || j > 35 && j < 38)
+                        {
+                            this.pixels[i, j].BackColor = Color.Black;
+                            this.pixels[i, j].FlatAppearance.BorderColor = Color.Black;
+                        }
+                    }
+                    if (i > 13 && i < 23)
+                    {
+                        if (j >= 0 && j < 2 || j > 36 && j <= 38)
+                        {
+                            this.pixels[i, j].BackColor = Color.Black;
+                            this.pixels[i, j].FlatAppearance.BorderColor = Color.Black;
+                        }
+                    }
+                }
+            }
+        }
+
+        private void btnEscudo_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i < this.filas; i++)
+            {
+                for (int j = 0; j < this.columnas; j++)
+                {
+                    if (i == 6 && j == 15)
+                    {
+                        this.pixels[i, j].BackColor = Color.Black;
+                        this.pixels[i, j].FlatAppearance.BorderColor = Color.Black;
+                    }
+                    if (i == 7 && (j > 13 && j < 17))
+                    {
+                        this.pixels[i, j].BackColor = Color.Black;
+                        this.pixels[i, j].FlatAppearance.BorderColor = Color.Black;
+                    }
+                    if (i == 8 && (j > 12 && j < 16 || j == 22))
+                    {
+                        this.pixels[i, j].BackColor = Color.Black;
+                        this.pixels[i, j].FlatAppearance.BorderColor = Color.Black;
+                    }
+                    if (i == 9 && (j > 11 && j < 24))
+                    {
+                        this.pixels[i, j].BackColor = Color.Black;
+                        this.pixels[i, j].FlatAppearance.BorderColor = Color.Black;
+                    }
+                    if (i == 10 && (j > 10 && j < 14 || j > 19 && j < 22))
+                    {
+                        this.pixels[i, j].BackColor = Color.Black;
+                        this.pixels[i, j].FlatAppearance.BorderColor = Color.Black;
+                    }
+                    if (i == 11 && ((j > 9 && j < 13 || j > 18 && j < 21 || j == 25)))
+                    {
+                        this.pixels[i, j].BackColor = Color.Black;
+                        this.pixels[i, j].FlatAppearance.BorderColor = Color.Black;
+                    }
+                    if (i == 12 && (j > 8 && j < 11 || j > 11 && j < 28))
+                    {
+                        this.pixels[i, j].BackColor = Color.Black;
+                        this.pixels[i, j].FlatAppearance.BorderColor = Color.Black;
+                    }
+                    if (i == 13 && (j > 7 && j < 10 || j > 11 && j < 14 || j > 17 && j < 20 || j > 24 && j < 27))
+                    {
+                        this.pixels[i, j].BackColor = Color.Black;
+                        this.pixels[i, j].FlatAppearance.BorderColor = Color.Black;
+                    }
+                    if (i == 14 && (j > 6 && j < 9 || j > 11 && j < 27))
+                    {
+                        this.pixels[i, j].BackColor = Color.Black;
+                        this.pixels[i, j].FlatAppearance.BorderColor = Color.Black;
+                    }
+                    if (i == 15 && (j > 5 && j < 8 || j > 11 && j < 14 || j > 17 && j < 20 || j > 24 && j < 27))
+                    {
+                        this.pixels[i, j].BackColor = Color.Black;
+                        this.pixels[i, j].FlatAppearance.BorderColor = Color.Black;
+                    }
+                    if (i == 16 && (j > 11 && j < 14 || j > 17 && j < 20 || j > 24 && j < 27))
+                    {
+                        this.pixels[i, j].BackColor = Color.Black;
+                        this.pixels[i, j].FlatAppearance.BorderColor = Color.Black;
+                    }
+                    if (i == 17 && (j > 11 && j < 27))
+                    {
+                        this.pixels[i, j].BackColor = Color.Black;
+                        this.pixels[i, j].FlatAppearance.BorderColor = Color.Black;
+                    }
+                    if (i == 18 && (j > 11 && j < 14 || j > 17 && j < 20 || j > 25 && j < 27))
+                    {
+                        this.pixels[i, j].BackColor = Color.Black;
+                        this.pixels[i, j].FlatAppearance.BorderColor = Color.Black;
+                    }
+                    if (i == 19 && (j == 10 || j > 17 && j < 20 || j == 27))
+                    {
+                        this.pixels[i, j].BackColor = Color.Black;
+                        this.pixels[i, j].FlatAppearance.BorderColor = Color.Black;
+                    }
+                    if (i == 20 && (j > 9 && j < 30))
+                    {
+                        this.pixels[i, j].BackColor = Color.Black;
+                        this.pixels[i, j].FlatAppearance.BorderColor = Color.Black;
+                    }
+                    if (i == 21 && (j > 9 && j < 12 || j > 17 && j < 20 || j > 26 && j < 29))
+                    {
+                        this.pixels[i, j].BackColor = Color.Black;
+                        this.pixels[i, j].FlatAppearance.BorderColor = Color.Black;
+                    }
+                    if (i == 22 && (j > 9 && j < 29))
+                    {
+                        this.pixels[i, j].BackColor = Color.Black;
+                        this.pixels[i, j].FlatAppearance.BorderColor = Color.Black;
+                    }
+                    if (i == 23 && (j > 9 && j < 12 || j > 17 && j < 20 || j > 26 && j < 29))
+                    {
+                        this.pixels[i, j].BackColor = Color.Black;
+                        this.pixels[i, j].FlatAppearance.BorderColor = Color.Black;
+                    }
+                    if (i == 24 && (j > 9 && j < 12 || j > 17 && j < 20 || j > 26 && j < 29))
+                    {
+                        this.pixels[i, j].BackColor = Color.Black;
+                        this.pixels[i, j].FlatAppearance.BorderColor = Color.Black;
+                    }
+                    if (i == 25 && (j > 9 && j < 29 || j == 31))
+                    {
+                        this.pixels[i, j].BackColor = Color.Black;
+                        this.pixels[i, j].FlatAppearance.BorderColor = Color.Black;
+                    }
+                    if (i == 26 && (j > 9 && j < 12 || j > 17 && j < 20 || j == 28 || j == 31))
+                    {
+                        this.pixels[i, j].BackColor = Color.Black;
+                        this.pixels[i, j].FlatAppearance.BorderColor = Color.Black;
+                    }
+                    if (i == 27 && (j > 17 && j < 20 || j == 31))
+                    {
+                        this.pixels[i, j].BackColor = Color.Black;
+                        this.pixels[i, j].FlatAppearance.BorderColor = Color.Black;
+                    }
+                    if (i == 28 && (j > 17 && j < 33))
+                    {
+                        this.pixels[i, j].BackColor = Color.Black;
+                        this.pixels[i, j].FlatAppearance.BorderColor = Color.Black;
+                    }
+                    if (i == 29 && (j > 17 && j < 32))
+                    {
+                        this.pixels[i, j].BackColor = Color.Black;
+                        this.pixels[i, j].FlatAppearance.BorderColor = Color.Black;
+                    }
+                }
+            }
+        }
+
+        private void btnBordar_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i < this.filas; i++)
+            {
+                for (int j = 0; j < this.columnas; j++)
+                {
+                    this.pixels[i, j].FlatAppearance.BorderColor = Color.Gray;
+                }
+            }
+        }
+    }
+}
